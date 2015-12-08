afe397fac8a1263a22977504419b0c68ed0a2437
Drew Noakes <git@drewnoakes.com>
2015-12-08 10:46:37 +0000

    Add beginnings of custom packer with tests.

---
+++ MsgPack.Strict.Tests/MsgPackPackerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace MsgPack.Strict.Tests
+{
+    public class MsgPackPackerTests
+    {
+        [Fact]
+        public void StreamWritePerfTest()
+        {
+            const int bufferSize = 1024;
+            const int chunkCount = 1024;
+
+            var s = new MemoryStream(bufferSize * chunkCount);
+
+            s.WriteByte(1);
+
+            var sw = Stopwatch.StartNew();
+
+            for (var i = 0; i < bufferSize * chunkCount; i++)
+                s.WriteByte((byte)i);
+
+            var oneByOneTime = sw.Elapsed.TotalMilliseconds;
+
+            s.Position = 0;
+
+            var buffer = Enumerable.Range(0, bufferSize).Select(i => (byte)i).ToArray();
+
+            sw.Restart();
+
+            for (var i = 0; i < chunkCount; i++)
+            {
+                for (int j = 0; j < buffer.Length; j++)
+                    buffer[j] = (byte)j;
+                s.Write(buffer, 0, bufferSize);
+            }
+
+            var inChunksTime = sw.Elapsed.TotalMilliseconds;
+
+            Console.Out.WriteLine("oneByOneTime = {0}", oneByOneTime);
+            Console.Out.WriteLine("inChunksTime = {0}", inChunksTime);
+        }
+
+        [Fact]
+        public void PacksInt()
+        {
+            var stream = new MemoryStream();
+            var packer = new MsgPackPacker(stream);
+            var unpacker = Unpacker.Create(stream);
+
+            for (int i = -60000; i < 60000; i++)
+            {
+                packer.Pack(i);
+
+                stream.Position = 0;
+
+                int result;
+                Assert.True(unpacker.ReadInt32(out result));
+                Assert.Equal(i, result);
+            }
+        }
+    }
+}
---
+++ MsgPack.Strict/MsgPackPacker.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MsgPack.Strict
+{
+    // TODO compare perf with MsgPack.Cli
+    // TODO compare perf if using intermediate buffer
+    // TODO compare perf if using unsafe code
+
+    public class MsgPackPacker
+    {
+        private readonly Stream _stream;
+
+        public MsgPackPacker(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public void Pack(bool value)
+        {
+            _stream.WriteByte(value ? (byte)0xC2 : (byte)0xC3);
+        }
+
+        public void PackNull()
+        {
+            _stream.WriteByte(0xc0);
+        }
+
+        public void PackArrayHeader(int length)
+        {
+            if (length < 0)
+                throw new ArgumentOutOfRangeException(nameof(length), length, "Must be zero or greater.");
+
+            if (length <= 0x0F)
+            {
+                _stream.WriteByte((byte)(0x90 | length));
+            }
+            else if (length <= 0xFFFF)
+            {
+                _stream.WriteByte(0xDC);
+                _stream.WriteByte((byte)(length >> 8));
+                _stream.WriteByte((byte)length);
+            }
+            else
+            {
+                _stream.WriteByte(0xDD);
+                _stream.WriteByte((byte)(length >> 24));
+                _stream.WriteByte((byte)(length >> 16));
+                _stream.WriteByte((byte)(length >> 8));
+                _stream.WriteByte((byte)length);
+            }
+        }
+
+        public void PackMapHeader(int length)
+        {
+            if (length < 0)
+                throw new ArgumentOutOfRangeException(nameof(length), length, "Must be zero or greater.");
+
+            if (length <= 0x0F)
+            {
+                _stream.WriteByte((byte)(0x80 | length));
+            }
+            else if (length <= 0xFFFF)
+            {
+                _stream.WriteByte(0xDE);
+                _stream.WriteByte((byte)(length >> 8));
+                _stream.WriteByte((byte)length);
+            }
+            else
+            {
+                _stream.WriteByte(0xDF);
+                _stream.WriteByte((byte)(length >> 24));
+                _stream.WriteByte((byte)(length >> 16));
+                _stream.WriteByte((byte)(length >> 8));
+                _stream.WriteByte((byte)length);
+            }
+        }
+
+        public void PackBinary(byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            if (bytes.Length <= 0xFF)
+            {
+                _stream.WriteByte(0xC4);
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+            else if (bytes.Length <= 0xFFFF)
+            {
+                _stream.WriteByte(0xC5);
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+            else
+            {
+                _stream.WriteByte(0xC6);
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        public void PackString(string value)
+        {
+            PackString(value, Encoding.UTF8);
+        }
+
+        public void PackString(string value, Encoding encoding)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            if (encoding == null)
+                throw new ArgumentNullException(nameof(encoding));
+
+            var bytes = encoding.GetBytes(value);
+
+            if (bytes.Length <= 0x1F)
+            {
+                _stream.WriteByte((byte)(0xA0 | bytes.Length));
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+            else if (bytes.Length <= 0xFF)
+            {
+                _stream.WriteByte(0xD9);
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+            else if (bytes.Length <= 0xFFFF)
+            {
+                _stream.WriteByte(0xDA);
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+            else
+            {
+                _stream.WriteByte(0xDB);
+                _stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        public void Pack(float value)
+        {
+            _stream.WriteByte(0xCA);
+            _stream.Write(BitConverter.GetBytes(value), 0, 4);
+        }
+
+        public void Pack(double value)
+        {
+            _stream.WriteByte(0xCB);
+            var l = BitConverter.DoubleToInt64Bits(value);
+            _stream.WriteByte((byte)(l >> 56));
+            _stream.WriteByte((byte)(l >> 48));
+            _stream.WriteByte((byte)(l >> 40));
+            _stream.WriteByte((byte)(l >> 32));
+            _stream.WriteByte((byte)(l >> 24));
+            _stream.WriteByte((byte)(l >> 16));
+            _stream.WriteByte((byte)(l >> 8));
+            _stream.WriteByte((byte)l);
+        }
+
+        public void Pack(byte value)
+        {
+            Pack((uint)value);
+        }
+
+        private void Pack(uint value)
+        {
+            if (value <= 0x7F)
+            {
+                // positive fixnum (7-bit positive number)
+                _stream.WriteByte((byte)value);
+            }
+            else if (value <= byte.MaxValue)
+            {
+                _stream.WriteByte(0xCC);
+                _stream.WriteByte((byte)value);
+            }
+            else if (value <= ushort.MaxValue)
+            {
+                _stream.WriteByte(0xCD);
+                _stream.WriteByte((byte)(value >> 8));
+                _stream.WriteByte((byte)value);
+            }
+            else // if (value <= uint.MaxValue)
+            {
+                _stream.WriteByte(0xCE);
+                _stream.WriteByte((byte)(value >> 24));
+                _stream.WriteByte((byte)(value >> 16));
+                _stream.WriteByte((byte)(value >> 8));
+                _stream.WriteByte((byte)value);
+            }
+        }
+
+        private void Pack(int value)
+        {
+            if (value >= 0x00 && value <= sbyte.MaxValue)
+            {
+                // positive fixnum (7-bit positive number)
+                _stream.WriteByte((byte)value);
+            }
+            else if (value >= -32 /*0b_1110_000*/ && value < 0x00)
+            {
+                // negative fixnum (5-bit negative number)
+                _stream.WriteByte((byte)(value | 0xE0));
+            }
+            else if (value >= sbyte.MinValue && value <= sbyte.MaxValue)
+            {
+                _stream.WriteByte(0xD0);
+                _stream.WriteByte((byte)value);
+            }
+            else if (value >= short.MinValue && value <= short.MaxValue)
+            {
+                _stream.WriteByte(0xD1);
+                _stream.WriteByte((byte)(value >> 8));
+                _stream.WriteByte((byte)value);
+            }
+            else // if (value >= int.MinValue && value <= int.MaxValue)
+            {
+                _stream.WriteByte(0xD2);
+                _stream.WriteByte((byte)(value >> 24));
+                _stream.WriteByte((byte)(value >> 16));
+                _stream.WriteByte((byte)(value >> 8));
+                _stream.WriteByte((byte)value);
+            }
+        }
+    }
+}
